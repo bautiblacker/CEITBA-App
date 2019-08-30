d6d9352dc8ccafa6d7fa1000bd1e3782eafbacbd
Udemy Rails Course <mcraviotto@itba.edu.ar>
2019-08-30 16:03:46 -0300

    first commit

---
+++ Bencraft/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Bencraft
+//
+//  Created by Martin Craviotto on 8/30/19.
+//  Copyright © 2019 Martin Craviotto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
