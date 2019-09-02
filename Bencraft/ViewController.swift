c1d158c7471daa5d1f5c4fd1563ab9dfaf7390ec
Udemy Rails Course <mcraviotto@itba.edu.ar>
2019-09-02 17:14:40 -0300

    mail feature working

--- Bencraft/ViewController.swift
+++ Bencraft/ViewController.swift
@@ -15,6 +15,31 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func sendEmail(_ sender: UIButton) {
+        
+        let request: NSMutableURLRequest = NSMutableURLRequest(url: NSURL(string: "https://api.mailgun.net/v3/sandbox0f8417ed9c56413a821c30b4be88f3ef.mailgun.org/messages")! as URL)
+        request.httpMethod = "POST"
+        
+        // Basic Authentication
+        let username = "api"
+        let password = "7be009135447af0018f7f3f94fd8855d-4167c382-8accc436"
+        let loginString = NSString(format: "%@:%@", username, password)
+        let loginData: NSData = loginString.data(using: String.Encoding.utf8.rawValue)! as NSData
+        let base64LoginString = loginData.base64EncodedString(options: [])
+        request.setValue("Basic \(base64LoginString)", forHTTPHeaderField: "Authorization")
+        
+        let bodyStr = "from=CeitbAPP <mailgun@sandbox0f8417ed9c56413a821c30b4be88f3ef.mailgun.org>&to=Ceitba <mcraviotto@itba.edu.ar>&subject=Notificación&text=Hubo una modificación para el alumno: Martin Craviotto. \nSe desea inscribir en: Gimnasio"
+        
+        // appending the data
+        request.httpBody = bodyStr.data(using: String.Encoding.utf8);
+        
+        let task = URLSession.shared.dataTask(with: request as URLRequest, completionHandler: { (data, response, error) -> Void in
+            print(response)
+        })
+        
+        task.resume()
 
+    }
+    
 }
 
