2b3ab263a5dff2074c4d6bb53bae3d9cddfa6d8e
Udemy Rails Course <mcraviotto@itba.edu.ar>
2019-09-02 23:54:15 -0300

    Firebase set up

--- Bencraft/ViewController.swift
+++ Bencraft/ViewController.swift
@@ -7,12 +7,21 @@
 //
 
 import UIKit
+import FirebaseDatabase
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+//        let ref = Database.database().reference()
+        
+//        ref.child("userid").setValue("bla", forKey: "sss") Puedo setear/agregar valores a la DB asi
+//        ref.child("userid").observeSingleEvent(of: .value)
+//        { (snapshot) in
+//            let name = snapshot.value as? String
+//        } Asi puedo leer los valores de la DB
     }
 
 
